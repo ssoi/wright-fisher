5d25db58985da85556e5d722dcae8d80e5f46878
Sameer Soi <sameersoi@gmail.com>
2013-12-14 21:07:23 -0500

    Redefined likelihood such that it incorporates information from all summary statistics

--- wf_mcmc.R
+++ wf_mcmc.R
@@ -1,32 +1,37 @@
-library(truncnorm)
+library(tmvtnorm, quietly=TRUE)
 source("wf_sim.R")
 
 # density of truncated normal
 dtnorm <- function(x, mean, sd, a, b) 
 	dnorm(x, mean, sd)/(sd*(pnorm(b, mean, sd) - pnorm(a, mean, sd)))
 
+# return coefficients from linear model
+lmr <- function(y, x) {
+	y <- ifelse(y == 0, min(y)/10, y)
+	Y <- log(y^2) 
+	X <- cbind(rep(1, length(x)), x, x^2)
+	b <- (solve(t(X) %*% X) %*% t(X)) %*% Y 
+	Yhat <- X %*% b
+	res <- exp(Y) - as.numeric(exp(Yhat))
+	coef <- list(C0=exp(b[1]), C1=b[2], C2=b[3])
+	return(list(Y=exp(as.numeric(Yhat)), res=res, coef=coef))
+}
+
 # propose new parameters for Metropolis-Hastings
 wf_mh_propose <- function(param, cntl) {	
 	proposal <- param
 	if(cntl$currIter %% 4 == 1) {
-		proposal$M1 <- rtruncnorm(n=1, mean=param$M1, sd=cntl$sigma.M, 
-			a=0, b=1)
-		#proposal$M1 <- rtruncnorm(n=1, mu=param$M1, sigma=cntl$sigma.M, 
-		#	lower=0, upper=1)
+		proposal$M1 <-	rtmvnorm(n=1, mean=param$M1, sigma=cntl$sigma.M^2,
+			upper=1, lower=0)
 	} else if(cntl$currIter %% 4 == 2) {
-		proposal$M2 <- rtruncnorm(n=1, mean=param$M2, sd=cntl$sigma.M, 
-			a=0, b=1)
-		#proposal$M2 <- rtruncnorm(n=1, mu=param$M2, sigma=cntl$sigma.M, 
-		#	lower=0, upper=1)
-	} else if(cntl$currIter %% 4 == 2) {
-		proposal$M1 <- rtruncnorm(n=1, mean=param$M1, sd=cntl$sigma.M, 
-			a=0, b=1)
+		proposal$M2 <-	rtmvnorm(n=1, mean=param$M2, sigma=cntl$sigma.M^2,
+			upper=1, lower=0)
 	} else if(cntl$currIter %% 4 == 3) {
-		proposal$Ne1 <- rtruncnorm(n=1, mean=param$Ne1, sd=cntl$sigma.M, 
-			a=1e2, b=1e5)
+		proposal$Ne1 <- rtmvnorm(n=1, mean=param$Ne1, sigma=cntl$sigma.Ne^2, 
+			upper=1e5, lower=1e2)
 	} else if(cntl$currIter %% 4 == 0) {
-		proposal$Ne2 <- rtruncnorm(n=1, mean=param$Ne2, sd=cntl$sigma.M, 
-			a=1e2, b=1e5)
+		proposal$Ne2 <- rtmvnorm(n=1, mean=param$Ne2, sigma=cntl$sigma.Ne^2, 
+			upper=1e5, lower=1e2)
 	}
 	return(proposal)
 }
@@ -38,33 +43,24 @@ wf_mh_step <- function(obs, curr, prev, cntl, init=F) {
 	ne <- c(curr$Ne1, curr$Ne1, curr$Ne2)
 	m <- c(curr$M1, curr$M2)
 	a <- c(curr$alpha1, curr$alpha2, curr$alpha3, curr$alpha4)
+	# simulate data with proposed parameters
 	sim <- wf_sim(param=list(T=t, B=b, Ne=ne, A=a, M=m))
-	if(cntl$currIter %% 2 == 1) {			
-		model <- nls(A ~ C0*exp(-C1*r-C2*r^2), data=data.frame(A=sim$A1, r=rec),
-			start=list(C0=1, C1=1, C2=1))
-		curr$loglik <- dnorm(x=sum(obs$A1 - fitted(model)), mean=0,
-			sd=sd(sim$A1 - fitted(model)), log=TRUE)
+	# fit curve and calculate likelihood based on multivariate normal
+	fit <- lapply(sim, lmr, x=rec)
+	res <- simplify2array(obs)^2 - simplify2array(sapply(fit, "[[", "Y"))
+	sigma <- cov(simplify2array(sapply(fit, "[[", "res")))
+	curr$loglik <- dmvnorm(x=colMeans(res), mean=rep(0, 4), sigma=sigma, log=T)
+	#	calculate appropriate transition densities
+	if(cntl$currIter %% 4 == 1) {
 		q01 <- log(dtnorm(x=curr$M1, mean=prev$M1, sd=cntl$sigma.M, a=0, b=1))
 		q10 <- log(dtnorm(x=prev$M1, mean=curr$M1, sd=cntl$sigma.M, a=0, b=1))
 	} else if(cntl$currIter %% 4 == 2) {
-		model <- nls(A ~ C0*exp(-C1*r-C2*r^2), data=data.frame(A=sim$A2, r=rec),
-			start=list(C0=1, C1=1, C2=1))
-		curr$loglik <- dnorm(x=sum(obs$A2 - fitted(model)), mean=0,
-			sd=sd(sim$A2 - fitted(model)), log=TRUE)
 		q01 <- log(dtnorm(x=curr$M2, mean=prev$M2, sd=cntl$sigma.M, a=0, b=1))
 		q10 <- log(dtnorm(x=prev$M2, mean=curr$M2, sd=cntl$sigma.M, a=0, b=1))
 	} else if(cntl$currIter %% 4 == 3) {	
-		model <- nls(D ~ C0*exp(-C1*r-C2*r^2), data=data.frame(A=sim$D1^2, r=rec),
-			start=list(C0=1, C1=1, C2=1))
-		curr$loglik <- dnorm(x=sum(obs$D1^2 - fitted(model)), mean=0,
-			sd=sd(sim$D1^2 - fitted(model)), log=TRUE)
 		q01 <- log(dtnorm(x=curr$Ne1, mean=prev$Ne1, sd=cntl$sigma.Ne, a=0, b=1))
 		q10 <- log(dtnorm(x=prev$Ne1, mean=curr$Ne1, sd=cntl$sigma.Ne, a=0, b=1))	
 	} else if(cntl$currIter %% 4 == 0) {	
-		model <- nls(D ~ C0*exp(-C1*r-C2*r^2), data=data.frame(A=sim$D2^2, r=rec),
-			start=list(C0=1, C1=1, C2=1))
-		curr$loglik <- dnorm(x=sum(obs$D2^2 - fitted(model)), mean=0,
-			sd=sd(sim$D2^2 - fitted(model)), log=TRUE)
 		q01 <- log(dtnorm(x=curr$Ne2, mean=prev$Ne2, sd=cntl$sigma.Ne, a=0, b=1))
 		q10 <- log(dtnorm(x=prev$Ne2, mean=curr$Ne2, sd=cntl$sigma.Ne, a=0, b=1))
 	}
@@ -85,6 +81,7 @@ pod <- wf_sim(param=list(T=c(1L, 150L, 200L), B=20L, Ne=c(1e4L, 1e4L, 3e3L),
 
 # define control for control MCMC	
 cntl <- list() # proposal, log-likelihood, and other deets for MCMC
+cntl$numIters <- 1e4 # length of chain
 cntl$currIter <- 1 # current iteration of MCMC chain
 cntl$B <- 20L # number of replicates per simulation
 cntl$sigma.M <- 0.01 # s.d. for migration proposal
@@ -93,24 +90,24 @@ cntl$sigma.Tm <- 1 # s.d. for time of migration proposal
 cntl$sigma.Tdiv <- 1 # s.d. for time of divergence proposal
 
 # define parameters for posterior sampling 
-numIters <- 1e4 # length of chain
+
 param <- data.frame(
-	Ne1=rep(NA, numIters), 
-	Ne2=rep(NA, numIters), 
-	M1=rep(NA, numIters), 
-	M2=rep(NA, numIters), 
-	alpha1=rep(NA, numIters), 
-	alpha2=rep(NA, numIters), 
-	alpha3=rep(NA, numIters), 
-	alpha4=rep(NA, numIters), 
-	Tmig=rep(NA, numIters), 
-	Tdiv=rep(NA, numIters),
-	loglik=rep(NA, numIters)
+	Ne1=rep(NA, cntl$numIters), 
+	Ne2=rep(NA, cntl$numIters), 
+	M1=rep(NA, cntl$numIters), 
+	M2=rep(NA, cntl$numIters), 
+	alpha1=rep(NA, cntl$numIters), 
+	alpha2=rep(NA, cntl$numIters), 
+	alpha3=rep(NA, cntl$numIters), 
+	alpha4=rep(NA, cntl$numIters), 
+	Tmig=rep(NA, cntl$numIters), 
+	Tdiv=rep(NA, cntl$numIters),
+	loglik=rep(NA, cntl$numIters)
 )
 
 # initialize MCMC chain
-param$Ne1[1] <- 1e4L
-param$Ne2[1] <- 3e3L
+param$Ne1[1] <- 1e3L
+param$Ne2[1] <- 1e3L
 param$M1[1] <- param$M2[1] <- 0
 param$alpha1[1] <- param$alpha4[1] <- 2
 param$alpha2[1] <- param$alpha3[1] <- 1
@@ -154,32 +151,33 @@ pod <- wf_sim(param=list(T=c(1L, 150L, 200L), B=20L, Ne=c(1e4L, 1e4L, 3e3L),
 
 # define control for control MCMC	
 cntl <- list() # proposal, log-likelihood, and other deets for MCMC
+cntl$numIters <- 1e4 # length of chain
 cntl$currIter <- 1 # current iteration of MCMC chain
 cntl$B <- 20L # number of replicates per simulation
 cntl$sigma.M <- 0.01 # s.d. for migration proposal
-cntl$sigma.Ne <- 0.001 # s.d. for migration proposal
+cntl$sigma.Ne <- 1e4 # s.d. for migration proposal
 cntl$sigma.Tm <- 1 # s.d. for time of migration proposal
 cntl$sigma.Tdiv <- 1 # s.d. for time of divergence proposal
+cntl$acc <- logical(cntl$numIters-1) # acceptance
 
 # define parameters for posterior sampling 
-numIters <- 1e4 # length of chain
 param <- data.frame(
-	Ne1=rep(NA, numIters), 
-	Ne2=rep(NA, numIters), 
-	M1=rep(NA, numIters), 
-	M2=rep(NA, numIters), 
-	alpha1=rep(NA, numIters), 
-	alpha2=rep(NA, numIters), 
-	alpha3=rep(NA, numIters), 
-	alpha4=rep(NA, numIters), 
-	Tmig=rep(NA, numIters), 
-	Tdiv=rep(NA, numIters),
-	loglik=rep(NA, numIters)
+	Ne1=rep(NA, cntl$numIters), 
+	Ne2=rep(NA, cntl$numIters), 
+	M1=rep(NA, cntl$numIters), 
+	M2=rep(NA, cntl$numIters), 
+	alpha1=rep(NA, cntl$numIters), 
+	alpha2=rep(NA, cntl$numIters), 
+	alpha3=rep(NA, cntl$numIters), 
+	alpha4=rep(NA, cntl$numIters), 
+	Tmig=rep(NA, cntl$numIters), 
+	Tdiv=rep(NA, cntl$numIters),
+	loglik=rep(NA, cntl$numIters)
 )
 
 # initialize MCMC chain
-param$Ne1[1] <- 1e4L
-param$Ne2[1] <- 3e3L
+param$Ne1[1] <- 1e3L
+param$Ne2[1] <- 1e3L
 param$M1[1] <- param$M2[1] <- 0
 param$alpha1[1] <- param$alpha4[1] <- 2
 param$alpha2[1] <- param$alpha3[1] <- 1
@@ -190,11 +188,11 @@ param$Tdiv[1] <- 200L
 param[1,] <- wf_mh_step(obs=pod, curr=param[1,], prev=param[1,], cntl=cntl, init=T)
 
 # run chain
-for(iter in 3556:numIters) {
+for(iter in 223:cntl$numIters) {
 	cntl$currIter <- iter
-	proposal <- try(wf_mh_propose(param[iter-1,], cntl))
 	repeat {
-		mh <- wf_mh_step(obs=pod, curr=proposal, prev=param[iter-1,], cntl=cntl)
+		proposal <- wf_mh_propose(param[iter-1,], cntl)
+		mh <- try(wf_mh_step(obs=pod, curr=proposal, prev=param[iter-1,], cntl=cntl))
 		if(class(mh) != "try-error") break
 	}	
 	if(mh$acc) {
@@ -202,7 +200,8 @@ for(iter in 3556:numIters) {
 	} else {
 		param[iter,] <- param[iter-1,] 
 	}
-	if(iter %% 100 == 0)
-		print(paste(iter, ":", proposal$M1, proposal$M2, "|", 
-			paste(param[iter,], collapse=" ")))
+	cntl$acc[iter-1] <- mh$acc
+	if(iter %% 20 == 0)
+		print(paste(iter, ":", paste(round(proposal[1:4],3), collapse=" "), "|", 
+			paste(round(param[iter,c(1:4, 11)],3), collapse=" ")))
 }
